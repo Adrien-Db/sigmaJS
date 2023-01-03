16b8f4b13d72275fee637463833341a477c47a59
Adrien-Db <73785397+Adrien-Db@users.noreply.github.com>
2023-01-03 12:38:49 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+1. npm install ----> install all dependencies (package.json --> node_modules)
+2. npm run dev ----> launch server on local host
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import { nextTick, ref, render } from 'vue'
+import Graph from 'graphology'
+import Sigma from 'sigma'
+import { Coordinates, EdgeDisplayData, NodeDisplayData } from "sigma/types"
+import data from "../assets/graph.json"
+
+const container = ref();
+const graph = new Graph();
+graph.import(data);
+let sigma: Sigma;
+nextTick(() => {
+  sigma = new Sigma(graph, container.value as HTMLElement, {
+    //minCameraRatio: 0.1,
+    //maxCameraRatio: 10,
+  })
+});
+
+
+
+</script>
+
+<template>
+  <div ref="container" class="container"></div>
+</template>
+
+<style>
+.container {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  border: 2px solid black;
+}
+
+</style>
